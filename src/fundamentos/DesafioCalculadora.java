eac4c0725088ec34fdfdc0fc0244c3abb6ef019b
wandovilar <wandovilar@outlook.com>
2026-02-12 22:09:04 -0300

    "Atualizando Estudos: Operadores Aritmeticos-Operadores"

---
+++ src/fundamentos/DesafioCalculadora.java
@@ -0,0 +1,32 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioCalculadora {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Informe o número: ");
+        double num1 = entrada.nextDouble();
+
+        System.out.print("Informe o número: ");
+        double num2 = entrada.nextDouble();
+
+
+        System.out.print("Informe a operação: ");
+        String op = entrada.next();
+
+
+        double result = "+".equals(op) ? num1 + num2 : 0;
+        result = "-".equals(op) ? num1 - num2 : result;
+        result = "*".equals(op) ? num1 * num2 : result;
+        result = "/".equals(op) ? num1 / num2 : result;
+        result = "%".equals(op) ? num1 % num2 : result;
+
+        System.out.printf("Seu calculo de %.2f %s %.2f = %.2f", num1, op, num2, result);
+
+        entrada.close();
+    }
+}
--- src/fundamentos/operadores/DesafioAritmeticos.java
+++ src/fundamentos/operadores/DesafioAritmeticos.java
@@ -4,7 +4,7 @@ public class DesafioAritmeticos {
     public static void main(String[] args) {
 
         double numA = Math.pow(6 * (3 + 2), 2);
-        double denA =  3 * 2;
+        double denA = 3 * 2;
 
 
         double numB = (1 - 5) * (2 - 7);
